6a389b399134dd9fbf3c46e65f1216a5fe30f803
Franz-Filip Schörghuber <S1910307091@students.fh-hagenberg.at>
2022-01-25 21:47:12 +0100

    added models; added userservice; added tags dependency to display list of tags; added intl dependency to format dateTime

---
+++ sma_app/lib/services/userservice.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:sma_app/models/tagable.dart';
+import 'package:sma_app/models/user.dart';
+
+class UserService {
+  List<User> users = [];
+  UserService(){
+    createUsers();
+  }
+
+  // User createUser(){
+  //   return User()
+  // }
+
+  List<TagAble> createHobbies(){
+    List<TagAble> hobbies = [];
+    hobbies.add(TagAble("Tanzen", Colors.lightGreen[300]!));
+    hobbies.add(TagAble("Tanzen", Colors.lightGreen[400]!));
+    hobbies.add(TagAble("Fußball", Colors.lightGreen[400]!));
+    hobbies.add(TagAble("Eislaufen", Colors.lightGreen[400]!));
+    hobbies.add(TagAble("Skifahren", Colors.lightGreen[400]!));
+    hobbies.add(TagAble("Volleayball", Colors.lightGreen[400]!));
+    return hobbies;
+  }
+
+  List<TagAble> createAttributes(){
+    List<TagAble> attributes = [];
+    attributes.add(TagAble("Lesen", Colors.orange[300]!));
+    attributes.add(TagAble("Klavier spielen", Colors.orange[300]!));
+    attributes.add(TagAble("Singen", Colors.orange[300]!));
+    return attributes;
+  }
+
+  List<TagAble> createSkills(){
+    List<TagAble> skills = [];
+    skills.add(TagAble("klein", Colors.indigo[300]!));
+    skills.add(TagAble("süß", Colors.indigo[300]!));
+    skills.add(TagAble("liebenswürdig", Colors.indigo[300]!));
+    return skills;
+  }
+
+  createUsers(){
+    users.add(User(createHobbies(), "Reiner", "Zufall", "Schlafe, mein Prinzchen, es ruhn Schäfchen und Vögelchen nun. Garten und Wiese verstummt, Auch nicht ein Bienchen mehr summt; Luna mit silbernem Schein Gucket zum Fenster herein."
+              "Schlafe beim silbernen Schein, Schlafe, mein Prinzchen, schlaf' ein! Schlaf' ein, schlaf 'ein!", DateTime(1997, 12, 10), "Hagenberg", 1, createAttributes(), createSkills(), "männlich"));
+  }
+
+  User getUserById(int index){
+    return users[index];
+  }
+}
